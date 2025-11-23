0deeef3661dbbe243afc19fd560246bda9372b6a
ApsolutNS <alexsulca42@gmail.com>
2025-11-23 16:39:18 -0500

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyD4cFHDbSfJNAhTuuP01N5JZQd-FOYB2LM",
   authDomain: "feedback-app-ac30e.firebaseapp.com",
   projectId: "feedback-app-ac30e",
-  storageBucket: "feedback-app-ac30e.appspot.com",
+  storageBucket: "feedback-app-ac30e.firebasestorage.app",
   messagingSenderId: "512179147778",
   appId: "1:512179147778:web:795e4a8b177fe766d3431b",
 };

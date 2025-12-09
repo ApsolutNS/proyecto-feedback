ad424e76b96352cbdcd21d5055fb7a198c80bfb3
ApsolutNS <alexsulca42@gmail.com>
2025-12-09 13:55:13 -0500

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,25 +1,56 @@
-import { auth } from "./firebase.js";
-import { 
-  signInWithEmailAndPassword 
+import { auth } from "./js/firebase.js";
+import { db } from "./js/firebase.js";
+
+import {
+  signInWithEmailAndPassword,
+  onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
 
-document.getElementById("btnLogin").addEventListener("click", login);
+import {
+  doc,
+  getDoc
+} from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const msg = document.getElementById("msg");
+const btn = document.getElementById("btnLogin");
+
+btn.addEventListener("click", login);
 
 async function login() {
-  const email = document.getElementById("email").value.trim();
-  const pass = document.getElementById("password").value.trim();
-
-  const msg = document.getElementById("errorMsg");
-  msg.textContent = "";
-
+  msg.innerText = "Verificando...";
   try {
-    const user = await signInWithEmailAndPassword(auth, email, pass);
+    const userCred = await signInWithEmailAndPassword(auth, email.value, password.value);
+    const uid = userCred.user.uid;
 
-    // Redirección automática:
-    location.href = "index.html";
+    // Leer rol del usuario
+    const snap = await getDoc(doc(db, "usuarios", uid));
 
-  } catch (err) {
-    msg.textContent = "Credenciales inválidas";
-    console.error(err);
+    if (!snap.exists()) {
+      msg.innerText = "El usuario no tiene rol asignado";
+      return;
+    }
+
+    const rol = snap.data().rol;
+
+    // Redirección automática según rol
+    switch (rol) {
+      case "admin":
+      case "supervisor":
+        location.href = "index.html";
+        break;
+
+      case "agente":
+        location.href = "portal_agente.html";
+        break;
+
+      default:
+        msg.innerText = "Rol desconocido. Contacta al administrador.";
+    }
+
+  } catch (e) {
+    msg.innerText = "Credenciales incorrectas";
+    console.error(e);
   }
 }
